07115d915aad1d4d9b2254b0a00dadfbd0912f80
Yuheng Zhang <55777837+Betristor@users.noreply.github.com>
2024-03-26 15:46:19 +0800

    Add new products

--- utils/add_database.py
+++ utils/add_database.py
@@ -1,11 +1,14 @@
 import sqlite3
+from faker import Faker
 
-def add_product(path, block, shelf, level, product_name, price, stock):
+fake = Faker()
+
+def add_product(path, block, shelf, level, product_name, price, unit, stock, product_id):
     conn = sqlite3.connect(path)
     c = conn.cursor()
 
-    c.execute("INSERT INTO Products (Block, Shelf, Level, ProductName, Price, Stock) VALUES (?, ?, ?, ?, ?, ?)",
-              (block, shelf, level, product_name, price, stock))
+    c.execute("INSERT INTO Products (Block, Shelf, Level, ProductName, Price, Unit, Stock, ProductID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+              (block, shelf, level, product_name, price, unit, stock, product_id))
     conn.commit()
 
     conn.close()
@@ -17,7 +20,9 @@ shelf = "Shelf 5"
 level = "Level 2"
 product_name = "Banana"
 price = 1.99
+unit = "kg"
 stock = 50
+product_id = fake.uuid4()
 
-add_product(path, block, shelf, level, product_name, price, stock)
+add_product(path, block, shelf, level, product_name, price, unit, stock, product_id)
 print("Product added successfully.")
