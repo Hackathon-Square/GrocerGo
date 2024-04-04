49e65f6f6b15ed1d873e1a972f63eeb1f0d56662
Mokkieee <161286491+Mokkieee@users.noreply.github.com>
2024-04-04 23:14:31 +1100

    first commit for register & login
    tested and worked -> create user database from registering

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+$mysql = new mysqli("localhost", "root", "jbgsn787", "User");
+if ($mysql->connect_errno) {
+    die("Disconnection: " . $mysql->connect_error); 
+}
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+$name = $_POST["full_name"];
+
+if(isset($_POST["login"])){
+    $sql = "SELECT password_hash FROM User WHERE name = '$name'";
+    //搜索结果
+    $res = $mysql -> query($sql) -> fetch_assoc();
+
+    if($res){
+        if(password_verify($_POST["password"], $res["password_hash"])){
+            //if matched, go to next page(profile page? search page?)
+            echo "<script>window.location.href = 'profile.php?uname=$name';</script>"; // Fixed missing quotation mark and removed space in URL parameter
+        } else {
+            //if dismatched
+            echo "<script>alert('Incorrect Password! Please try again.'); history.go(-1);</script>"; // Fixed missing quotation mark
+        }
+    } else {
+        echo "<script>alert('You need to register first!'); history.go(-1);</script>"; // Fixed missing quotation mark
+    }
+} 
+?>
