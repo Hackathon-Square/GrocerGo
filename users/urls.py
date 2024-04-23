0a3a68b4c4394967d4c9238dd21d9de1360416eb
Yuheng Zhang <55777837+Betristor@users.noreply.github.com>
2024-04-23 13:31:50 +0800

    users

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path("", views.homepage, name="home"),
+    path("profile/", views.profile, name="profile"),
+    path("setting/", views.setting, name="setting"),
+    path("coupon/", views.coupon, name = "coupon"),
+    path("register/", views.register, name="register"),
+    path("login/", views.my_login, name="login"),
+    path("logout/", views.my_logout, name="logout"),
+]
---
+++ users/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+
+from .forms import UserRegisterForm, LoginForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import auth
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+
+
+def homepage(request):
+    return render(request, "users/homepage.html")
+
+
+@login_required(login_url="login")
+def profile(request):
+    return render(request, "users/profile.html")
+
+
+def register(request):
+    form = UserRegisterForm()
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            messages.error(request, form.errors)
+
+    contxet = {"form": form}
+
+    return render(request, "users/register.html", context=contxet)
+
+
+def my_login(request):
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                return redirect("login")
+
+    context = {"form": form}
+
+    return render(request, "users/login.html", context=context)
+
+
+def setting(request):
+    pass
+
+
+def coupon(request):
+    pass
+
+
+def my_logout(request):
+    logout(request)
+    return redirect("login")
