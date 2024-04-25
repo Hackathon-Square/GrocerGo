19324b33a37cb4982c1abae6b20f03bccbcfb884
Yuheng Zhang <55777837+Betristor@users.noreply.github.com>
2024-04-25 11:53:27 +0800

    Update giftcard info

--- users/activategiftcard.py
+++ users/activategiftcard.py
@@ -5,13 +5,15 @@ client = Client(access_token=os.environ["SQUARE_ACCESS_TOKEN"], environment="san
 
 result = client.gift_card_activities.create_gift_card_activity(
     body={
-        "idempotency_key": "100",
+        "idempotency_key": "001",
         "gift_card_activity": {
             "type": "ACTIVATE",
-            "location_id": "LBAKXA7BAEKHX",
-            "gift_card_gan": "001",
+            "location_id": "LGM75327C41RJ",
+            "gift_card_id": "gftc:2bcda6a90d524a6db268bbd72c82966a",
+            "gift_card_gan": "7783324663378794",
             "activate_activity_details": {
-                "amount_money": {"amount": 20, "currency": "AUD"}
+                "amount_money": {"amount": 20, "currency": "AUD"},
+                "buyer_payment_instrument_ids": ["first"],
             },
         },
     }
--- users/creategiftcard.py
+++ users/creategiftcard.py
@@ -5,9 +5,9 @@ client = Client(access_token=os.environ["SQUARE_ACCESS_TOKEN"], environment="san
 
 result = client.gift_cards.create_gift_card(
     body={
-        "idempotency_key": "100",
-        "location_id": "LBAKXA7BAEKHX",
-        "gift_card": {"type": "DIGITAL", "gan_source": "SQUARE", "gan": "001"},
+        "idempotency_key": "001",
+        "location_id": "LGM75327C41RJ",
+        "gift_card": {"type": "DIGITAL", "gan_source": "SQUARE", "gan": "giftcard001"},
     }
 )
 
